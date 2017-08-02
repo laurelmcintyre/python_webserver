37077c66a407e7ebe7670c9a809ef465495661f3
laurelmcintyre <lmcintyre@lanl.gov>
2017-08-02 10:25:24 -0600

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,54 @@
+import cgi
+import sqlite3
+
+form = b'''
+    <html>
+    <head>
+    <title>Translation</title>
+    </head>
+    <body>
+    <form method="post" action="">
+    <label>Translate</label>
+    <input type="text" name="textinput">
+    <input type="submit" value="Go">
+    </form>
+    </body>
+    </html>
+    '''
+
+
+def app(environ, start_response):
+    html = form
+
+    if environ['REQUEST_METHOD'] == 'POST':
+        post_env = environ.copy()
+        post_env['QUERY_STRING'] = ''
+        post = cgi.FieldStorage(
+                                fp=environ['wsgi.input'],
+                                environ=post_env,
+                                keep_blank_values=True
+                                )
+        query = "SELECT english || ' ' || spanish FROM Translation \
+        WHERE english=?;"
+        connection = sqlite3.connect('translation.db')
+        cursor = connection.cursor()
+        input_text = post.getlist("textinput")
+        cursor.execute(query, input_text)
+        results = cursor.fetchall()
+        print post
+        cursor.close()
+        connection.close()
+        print results
+        html = b'Translate, ' + str(results) + '.'
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return [html]
+
+
+if __name__ == '__main__':
+    try:
+        from wsgiref.simple_server import make_server
+        httpd = make_server('', 8080, app)
+        print('Serving on port 8080...')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('Goodbye.')
